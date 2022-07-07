d570d4f1d4b76a62d3f92e14b0a41e58f4727b42
18534989838 <107414249+18534989838@users.noreply.github.com>
2022-07-07 22:04:05 +0800

    Update test.java

--- HelloGit/src/main/java/test.java
+++ HelloGit/src/main/java/test.java
@@ -1,5 +1,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("git test 2022-07-07");
+        System.out.println("在github上修改代码,测试git的pull流程");
     }
 }
