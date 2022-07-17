8648fc79f7e68311d5dd088d33461f41c9d84e8f
18534989838 <107414249+18534989838@users.noreply.github.com>
2022-07-17 15:00:45 +0800

    新增flume模块

--- HelloGit/src/main/java/test.java
+++ HelloGit/src/main/java/test.java
@@ -3,5 +3,6 @@ public class test {
         System.out.println("git test 2022-07-07");
         System.out.println("在github上修改代码,测试git的pull流程");
         System.out.println("提交到ty分支上");
+        System.out.println("提交到ty分支上1");
     }
 }
