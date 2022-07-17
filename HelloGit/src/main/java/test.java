f24853150063cdd7db4aca1bf0359cb16840bac1
18534989838 <107414249+18534989838@users.noreply.github.com>
2022-07-17 14:54:46 +0800

    提交到分支

--- HelloGit/src/main/java/test.java
+++ HelloGit/src/main/java/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("git test 2022-07-07");
         System.out.println("在github上修改代码,测试git的pull流程");
+        System.out.println("提交到ty分支上");
     }
 }
